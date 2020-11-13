d39c641d5080c01ac64eb5210c7010072ffe27b9
geekon <kacperpiasta@gmail.com>
2020-11-13 21:23:09 +0100

    fix breadcrumb & bad styling

--- available_rooms.php
+++ available_rooms.php
@@ -1,1 +1,24 @@
 <?php
+include 'helpers/include_all.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<?php view('header.php'); ?>
+
+<body>
+<?php view('navbar.php'); ?>
+<div class="container-fluid">
+    <div class="row">
+        <?php view('sidebar.php'); ?>
+        <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+            <?php view('breadcrumb.php'); ?>
+            <p>View available rooms and search by predefined criteria</p>
+            <?php view('footer.php'); ?>
+        </main>
+    </div>
+</div>
+<?php view('sign_out_modal.php'); ?>
+
+</body>
+</html>
