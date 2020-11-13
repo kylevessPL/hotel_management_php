50e6ed51fbf989ba2e2828ea0f99f0a8e79e7ba2
geekon <kacperpiasta@gmail.com>
2020-11-13 22:41:30 +0100

    adjust styling for mobile devices

--- views/sidebar.php
+++ views/sidebar.php
@@ -2,48 +2,48 @@
 include 'helpers/page_info.php';
 ?>
 
-<nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light mt-2 sidebar collapse">
+<nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
     <div class="position-sticky">
         <ul class="nav flex-column">
-            <li class="nav-item mb-2">
+            <li class="nav-item">
                 <a href="/dashboard"
                    class=<?php echo $title == 'Dashboard' ? '"nav-link active" aria-current="page"' : '"nav-link"'; ?>>
-                    <i class="las la-home align-bottom" style="font-size: 32px;"></i>
+                    <i class="las la-home align-bottom" style="font-size: 32px; margin-right: 4px;"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
-            <li class="nav-item mb-2">
+            <li class="nav-item">
                 <a href="/dashboard/available-rooms"
                    class=<?php echo $title == 'Available rooms' ? '"nav-link active" aria-current="page"' : '"nav-link"'; ?>>
-                    <i class="las la-bed align-bottom" style="font-size: 32px;"></i>
+                    <i class="las la-bed align-bottom" style="font-size: 32px; margin-right: 4px;"></i>
                     <span>Available rooms</span>
                 </a>
             </li>
-            <li class="nav-item mb-2">
+            <li class="nav-item">
                 <a href="/dashboard/additional-services"
                    class=<?php echo $title == 'Additional services' ? '"nav-link active" aria-current="page"' : '"nav-link"'; ?>>
-                    <i class="las la-concierge-bell align-bottom" style="font-size: 32px;"></i>
+                    <i class="las la-concierge-bell align-bottom" style="font-size: 32px; margin-right: 4px;"></i>
                     <span>Additional services</span>
                 </a>
             </li>
-            <li class="nav-item mb-2">
+            <li class="nav-item">
                 <a href="/dashboard/book-room"
                    class=<?php echo $title == 'Book room' ? '"nav-link active" aria-current="page"' : '"nav-link"'; ?>>
-                    <i class="las la-user-clock align-bottom" style="font-size: 32px;"></i>
+                    <i class="las la-user-clock align-bottom" style="font-size: 32px; margin-right: 4px;"></i>
                     <span>Book room</span>
                 </a>
             </li>
-            <li class="nav-item mb-2">
+            <li class="nav-item">
                 <a href="/dashboard/my-bookings"
                    class=<?php echo $title == 'My bookings' ? '"nav-link active" aria-current="page"' : '"nav-link"'; ?>>
-                    <i class="las la-bookmark align-bottom" style="font-size: 32px;"></i>
+                    <i class="las la-bookmark align-bottom" style="font-size: 32px; margin-right: 4px;"></i>
                     <span>My bookings</span>
                 </a>
             </li>
-            <li class="nav-item mb-2">
+            <li class="nav-item">
                 <a href="/dashboard/payment-history"
                    class=<?php echo $title == 'Payment history' ? '"nav-link active" aria-current="page"' : '"nav-link"'; ?>>
-                    <i class="las la-file-invoice-dollar align-bottom" style="font-size: 32px;"></i>
+                    <i class="las la-file-invoice-dollar align-bottom" style="font-size: 32px; margin-right: 4px;"></i>
                     <span>Payment history</span>
                 </a>
             </li>
