8b96a3bb5e74d1198a058af475938346a9b1e6fb
geekon <kacperpiasta@gmail.com>
2020-11-14 01:59:49 +0100

    add about and contact site desc

--- about_us.php
+++ about_us.php
@@ -13,7 +13,7 @@ include 'helpers/include_all.php';
         <?php view('sidebar.php'); ?>
         <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
             <?php view('breadcrumb.php'); ?>
-            <p>About us</p>
+            <p>Find out more about our service</p>
             <?php view('footer.php'); ?>
         </main>
     </div>
--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ include 'helpers/include_all.php';
         <?php view('sidebar.php'); ?>
         <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
             <?php view('breadcrumb.php'); ?>
-            <p>Contact</p>
+            <p>Feel free to contact us if you have any any questions regarding our service</p>
             <?php view('footer.php'); ?>
         </main>
     </div>
