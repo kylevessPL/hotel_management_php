8ad6956e529ed716e9856894c3bd4f9dd6f7c706
geekon <kacperpiasta@gmail.com>
2020-11-25 00:53:31 +0100

    add address modal layout changes

--- my_addresses.php
+++ my_addresses.php
@@ -103,28 +103,28 @@ if (isset($customerId))
 <div id="addressRequest" class="addressRequest">
     <form method="post" id="address-form" name="address-form" action="/account/my-addresses">
         <div aria-hidden="true" aria-labelledby="addressModalLabel" class="modal fade" id="addressModal" role="dialog" tabindex="-1">
-            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-dialog modal-dialog-centered" style="max-width: 650px" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="addressModalLabel"></h5><button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
                     </div>
                     <div class="modal-body">
                         <div class="form-group row">
-                            <div class="col-sm-6">
+                            <div class="col-sm-8">
                                 <label class="control-label" for="streetName">Street name:</label>
                                 <input class="form-control" id="streetName" type="text" name="streetName" placeholder="Enter street name" autofocus>
                             </div>
-                            <div class="col-sm-6">
+                            <div class="col-sm-4">
                                 <label class="control-label" for="houseNumber">House number:</label>
-                                <input class="form-control" id="houseNumber" type="text" placeholder="Enter house number" name="houseNumber">
+                                <input class="form-control" id="houseNumber" type="text" placeholder="Your house number" name="houseNumber">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <div class="col-sm-6">
+                            <div class="col-sm-4">
                                 <label class="control-label" for="zipCode">Zip code:</label>
                                 <input class="form-control" id="zipCode" type="text" placeholder="Enter zip code" name="zipCode">
                             </div>
-                            <div class="col-sm-6">
+                            <div class="col-sm-8">
                                 <label class="control-label" for="city">City:</label>
                                 <input class="form-control" id="city" type="text" placeholder="Enter city" name="city">
                             </div>
