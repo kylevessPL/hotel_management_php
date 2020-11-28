d3e9f2cf1ac6e0cc6e73331d51684901341b5eb0
geekon <kacperpiasta@gmail.com>
2020-11-28 15:21:46 +0100

    cleanups

--- assets/js/available-rooms.js
+++ assets/js/available-rooms.js
@@ -75,7 +75,7 @@ function buildTable(startDate, endDate, bedAmount, amenities, minPrice, maxPrice
         order: [[ 2, 'asc' ]]
     });
     table.on( 'order.dt search.dt', function () {
-        table.column(0, {search: 'applied', order: 'applied'}).nodes().each( function (cell, i) {
+        table.column(0, { search: 'applied', order: 'applied' }).nodes().each( function (cell, i) {
             cell.innerHTML = i+1;
         });
     }).draw();
