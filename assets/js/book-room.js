980ef3599191e1c90dcfc06c839fcd0ba34c0c9e
geekon <kacperpiasta@gmail.com>
2020-12-06 15:33:53 +0100

    code cleanup

--- assets/js/book-room.js
+++ assets/js/book-room.js
@@ -139,38 +139,7 @@ function showPeopleOption(bedAmount) {
                 documentType.selectpicker('refresh');
                 documentType.selectpicker('setStyle', 'btn', 'remove');
                 documentType.selectpicker('setStyle', 'form-control');
-                $('.first-name').last().rules( "add", {
-                    required: true,
-                    minlength: 2,
-                    maxlength: 30,
-                    messages: {
-                        required: "First name is mandatory",
-                        minlength: "First name must be at least 8 characters long",
-                        maxlength: "First name must be maximum 30 characters long"
-                    }
-                });
-                $('.last-name').last().rules( "add", {
-                    required: true,
-                    minlength: 2,
-                    maxlength: 30,
-                    messages: {
-                        required: "Last name is mandatory",
-                        minlength: "Last name must be at least 8 characters long",
-                        maxlength: "Last name must be maximum 30 characters long"
-                    }
-                });
-                $('.document-id').last().rules( "add", {
-                    required: true,
-                    minlength: 7,
-                    maxlength: 14,
-                    regex: /^[A-Z0-9 -]*$/,
-                    messages: {
-                        required: "Document ID is mandatory",
-                        minlength: "Document ID must be at least 7 characters long",
-                        maxlength: "Document ID must be maximum 14 characters long",
-                        regex: "Document ID must contain only capital letters, digits or - character"
-                    }
-                });
+                setPeopleSectionClassRules();
             }
         });
     }
@@ -229,4 +198,39 @@ function updateTotal(value, op) {
         totalCount.html(parseInt(totalCount.html()) - 1);
     }
     total.html(result === 0 ? result : result.toFixed(2) + ' PLN');
+}
+
+function setPeopleSectionClassRules() {
+    $('.first-name').last().rules( "add", {
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+        messages: {
+            required: "First name is mandatory",
+            minlength: "First name must be at least 8 characters long",
+            maxlength: "First name must be maximum 30 characters long"
+        }
+    });
+    $('.last-name').last().rules( "add", {
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+        messages: {
+            required: "Last name is mandatory",
+            minlength: "Last name must be at least 8 characters long",
+            maxlength: "Last name must be maximum 30 characters long"
+        }
+    });
+    $('.document-id').last().rules( "add", {
+        required: true,
+        minlength: 7,
+        maxlength: 14,
+        regex: /^[A-Z0-9 -]*$/,
+        messages: {
+            required: "Document ID is mandatory",
+            minlength: "Document ID must be at least 7 characters long",
+            maxlength: "Document ID must be maximum 14 characters long",
+            regex: "Document ID must contain only capital letters, digits or - character"
+        }
+    });
 }
