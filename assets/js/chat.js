ac2d2f208d56c42c3424aac6a63cb64de78ffc29
geekon <kacperpiasta@gmail.com>
2020-12-24 16:28:53 +0100

    extend function timeout for removing chat anchor

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    setTimeout(() => $('a[href="https://www.messenger.com/t/2256078427869983"]').next().remove(), 500);
+    setTimeout(() => $('a[href="https://www.messenger.com/t/2256078427869983"]').next().remove(), 600);
 });
